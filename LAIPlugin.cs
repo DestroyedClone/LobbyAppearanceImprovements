2bfabcca1a25a4019521049ff1f1931720b79048
DestroyedClone <destroyedclone@gmail.com>
2021-08-16 04:19:17 -0700

    Added missing references

--- LAIPlugin.cs
+++ LAIPlugin.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using BepInEx;
+using BepInEx.Configuration;
+using LeTai.Asset.TranslucentImage;
+using R2API.Utils;
+using RoR2;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using static UnityEngine.ColorUtility;
 
 namespace LobbyAppearanceImprovements
 {
     public class LAIPlugin
     {
+        public const string ModVer = "1.1.0";
+        public const string ModName = "LobbyAppearanceImprovements";
+        public const string ModGuid = "com.DestroyedClone.LobbyAppearanceImprovements";
     }
 }
