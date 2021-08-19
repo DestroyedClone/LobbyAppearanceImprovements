b4a2a08d8c71fd96bf723ee226a909e94ed3fd80
DestroyedClone <destroyedclone@gmail.com>
2021-08-18 23:33:56 -0700

    codemaid

--- Scenes/LAIScene.cs
+++ Scenes/LAIScene.cs
@@ -1,29 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using UnityEngine;
-using RoR2;
-using R2API;
-using BepInEx;
-using BepInEx.Configuration;
-using R2API.Utils;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Runtime.CompilerServices;
-using EntityStates;
-using HG;
-using RoR2.Audio;
-using RoR2.Navigation;
-using RoR2.Networking;
-using RoR2.Orbs;
-using RoR2.Projectile;
-using RoR2.Skills;
-using RoR2.UI;
-using Unity;
-using UnityEngine.Events;
-using UnityEngine.Networking;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
 namespace LobbyAppearanceImprovements.Scenes
 {
