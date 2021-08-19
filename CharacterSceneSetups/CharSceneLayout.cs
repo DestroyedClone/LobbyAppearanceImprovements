fe21c4307cc192bbe9291cd88b8d3bc511411b23
DestroyedClone <destroyedclone@gmail.com>
2021-08-19 03:51:32 -0700

    setup for scene layouts

---
+++ CharacterSceneSetups/CharSceneLayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LobbyAppearanceImprovements.CharacterSceneSetups
+{
+    public abstract class CharSceneLayout
+    {
+        public abstract string SceneLayout { get; }
+        public abstract string SceneName { get; }
+        public abstract string Author { get; }
+        public abstract string LayoutName { get; }
+    }
+}
---
+++ CharacterSceneSetups/GooLake_DestroyedClone_Default.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LobbyAppearanceImprovements.CharacterSceneSetups
+{
+    public class GooLake_DestroyedClone_Default : CharSceneLayout
+    {
+        public override string SceneLayout => "GooLake_DestroyedClone_Default";
+        public override string SceneName => "GooLake";
+        public override string Author => "DestroyedClone";
+        public override string LayoutName => "Default";
+    }
+}
