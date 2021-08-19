9c4d92002ed3d0464d38a94159967c4dd60a639d
DestroyedClone <destroyedclone@gmail.com>
2021-08-18 22:18:23 -0700

    Added plugin attribute

--- LAIPlugin.cs
+++ LAIPlugin.cs
@@ -8,10 +8,17 @@ using System.Security;
 using System.Security.Permissions;
 using UnityEngine;
 using static UnityEngine.ColorUtility;
+[module: UnverifiableCode]
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
 
 namespace LobbyAppearanceImprovements
 {
-    public class LAIPlugin
+    [BepInDependency(R2API.R2API.PluginGUID, R2API.R2API.PluginVersion)]
+    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    public class LAIPlugin : BaseUnityPlugin
     {
         public const string ModVer = "1.1.0";
         public const string ModName = "LobbyAppearanceImprovements";
