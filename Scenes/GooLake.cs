67f91dbff3969627319b993b3ee5f85fef3dfc1e
DestroyedClone <destroyedclone@gmail.com>
2021-08-18 22:46:25 -0700

    Setup the structure for custom background scenes

---
+++ Scenes/GooLake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LobbyAppearanceImprovements.Scenes
+{
+    public class GooLake : LAIScene
+    {
+        public override string SceneName => "GooLake";
+        public override GameObject BackgroundPrefab => Resources.Load<GameObject>("prefabs/stagedisplay/GoolakeDioramaDisplay");
+        public override Vector3 Position => new Vector3();
+        public override Vector3 Rotation => new Vector3();
+    }
+}
---
+++ Scenes/LAIScene.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+using RoR2;
+using R2API;
+using BepInEx;
+using BepInEx.Configuration;
+using R2API.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using EntityStates;
+using HG;
+using RoR2.Audio;
+using RoR2.Navigation;
+using RoR2.Networking;
+using RoR2.Orbs;
+using RoR2.Projectile;
+using RoR2.Skills;
+using RoR2.UI;
+using Unity;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+using UnityEngine.Serialization;
+
+namespace LobbyAppearanceImprovements.Scenes
+{
+    public abstract class LAIScene
+    {
+        public abstract string SceneName { get; }
+        public abstract GameObject BackgroundPrefab { get; }
+        public abstract Vector3 Position { get; }
+        public abstract Vector3 Rotation { get; }
+    }
+}
