52347c55db40aa02ed60d3ecc65aa029e0b9d19f
DestroyedClone <destroyedclone@gmail.com>
2021-08-20 10:05:10 -0700

    updated CharacterLayouts to the new Dict

--- CharacterSceneLayouts/Lobby_DestroyedClone_Original.cs
+++ CharacterSceneLayouts/Lobby_DestroyedClone_Original.cs
@@ -13,53 +13,14 @@ namespace LobbyAppearanceImprovements.CharacterSceneLayouts
         public override string LayoutName => "Default";
         public override Dictionary<string, Vector3[]> CharacterLayouts => new Dictionary<string, Vector3[]>()
         {
+            { "Commando", new [] {new Vector3(2.65f, 0.01f, 6.00f), new Vector3(0f, 240f, 0f) } },
+            { "Huntress", new [] {new Vector3(2.33f, 0.01f, 7.4f), new Vector3(0f, 240f, 0f) } },
+            { "Bandit2", new [] {new Vector3(3.79f, 0.01f, 11.5f), new Vector3(0f, 240f, 0f) } },
+            { "Engi", new [] {new Vector3(0.2f, 0f, 20.4f), new Vector3(0f, 240f, 0f) } },
+            { "Merc", new [] {new Vector3(4.16f, 1.3f, 17f), new Vector3(0f, 240f, 0f) } },
+            { "Loader", new [] {new Vector3(-4f, 0f, 20.22f), new Vector3(0f, 140f, 0f) } },
+            { "Croco", new [] {new Vector3(-4f, 0f, 13f), new Vector3(0f, 120f, 0f) } },
 
-        };
-
-        public override CharacterLayouzt[] CharacterLayouts => new CharacterLayout[]
-        {
-            new CharacterLayout
-            {
-                BodyName = "Commando",
-                Position = new Vector3 (2.65f, 0.01f, 6.00f),
-                Rotation = new Vector3 (0f, 240f, 0f)
-            },
-            new CharacterLayout
-            {
-                BodyName = "Huntress",
-                Position = new Vector3 (2.33f, 0.01f, 7.4f),
-                Rotation = new Vector3 (0f, 240f, 0f)
-            },
-            new CharacterLayout
-            {
-                BodyName = "Bandit2",
-                Position = new Vector3 (3.79f, 0.01f, 11.5f),
-                Rotation = new Vector3 (0f, 240f, 0f)
-            },
-            new CharacterLayout
-            {
-                BodyName = "Engi",
-                Position = new Vector3 (0.2f, 0f, 20.4f),
-                Rotation = new Vector3 (0f, 240f, 0f)
-            },
-            new CharacterLayout
-            {
-                BodyName = "Merc",
-                Position = new Vector3 (4.16f, 1.3f, 17f),
-                Rotation = new Vector3 (0f, 240f, 0f)
-            },
-            new CharacterLayout
-            {
-                BodyName = "Loader",
-                Position = new Vector3 (-4f, 0f, 20.22f),
-                Rotation = new Vector3 (0f, 140f, 0f)
-            },
-            new CharacterLayout
-            {
-                BodyName = "Croco",
-                Position = new Vector3 (-4f, 0f, 13f),
-                Rotation = new Vector3 (0f, 120f, 0f)
-            },
         };
     }
 }
