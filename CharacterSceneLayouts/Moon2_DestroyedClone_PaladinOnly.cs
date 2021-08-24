db3dd3494f78d37a31fabfdf828dbdf99ebd09c4
DestroyedClone <destroyedclone@gmail.com>
2021-08-23 20:22:58 -0700

    adjusted the paladin one again

--- CharacterSceneLayouts/Moon2_DestroyedClone_PaladinOnly.cs
+++ CharacterSceneLayouts/Moon2_DestroyedClone_PaladinOnly.cs
@@ -39,7 +39,9 @@ namespace LobbyAppearanceImprovements.CharacterSceneLayouts
             var moonTexture = GameObject.Find("MoonDioramaDissplay(Clone)/MoonBridgeCornerWithTerrain/Terrain").GetComponent<MeshRenderer>().sharedMaterial;
             var model = Methods.CreateDisplay("RobPaladin", new Vector3(0, 0.5f, 5f), new Vector3(0,180,0));
             model.GetComponent<CharacterModel>().enabled = false;
+            model.GetComponent<Animator>().speed = 99;
             model.transform.Find("meshPaladin").gameObject.GetComponent<SkinnedMeshRenderer>().material = moonTexture;
+            model.transform.Find("meshSword").gameObject.GetComponent<SkinnedMeshRenderer>().material = moonTexture;
             model.transform.Find("Armature").gameObject.SetActive(false);
             model.name = "PaladinStatue";
             //PlayAnimationOnAnimator(anim, "Body", "SpawnClay", "Spawn.playbackRate", 3f);
@@ -55,6 +57,8 @@ namespace LobbyAppearanceImprovements.CharacterSceneLayouts
             GameObject.Destroy(model);
             var displayModel = Methods.CreateDisplay("RobPaladin", new Vector3(0, 0.5f, 5f), new Vector3(0, 180, 0), null, true);
             var animator = displayModel.GetComponent<Animator>();
+
+            // warning does not survive the layout being turned off and on, only works on first layout
             animator.runtimeAnimatorController = SurvivorCatalog.GetSurvivorDef(SurvivorCatalog.FindSurvivorIndex("RobPaladin")).bodyPrefab?.GetComponentInChildren<Animator>().runtimeAnimatorController;
             EntityStates.EntityState.PlayAnimationOnAnimator(animator, "Body", "Spawn", "Spawn.playbackRate", 3f);
             animator.speed = 0;
