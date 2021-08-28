7951c57060babcf9c6ae47fc85a65e841998c540
DestroyedClone <destroyedclone@gmail.com>
2021-08-28 01:09:43 -0700

    zz

--- CharacterSceneLayouts/Arena_DestroyedClone_Nemesis.cs
+++ CharacterSceneLayouts/Arena_DestroyedClone_Nemesis.cs
@@ -14,14 +14,14 @@ namespace LobbyAppearanceImprovements.CharacterSceneLayouts
         public override string LayoutName => "Shadow of the Nemesis";
         public override Dictionary<string, Vector3[]> CharacterLayouts => new Dictionary<string, Vector3[]>()
         {
-            { "Commando", new [] {new Vector3(2.65f, 0.01f, 6.00f), new Vector3(0f, 240f, 0f) } },
-            { "Nemmando", new [] {new Vector3(3.37f, 0.1f, 5.68f), new Vector3(0f, 214f, 0f) } },
+            { "Commando", new [] {new Vector3(0f, 0f, 7f), new Vector3(0f, 60f, 0f) } },
+            { "Nemmando", new [] {new Vector3(-0.3f, 0f, 6.6f), new Vector3(0f, 214f, 0f) } },
             //
-            { "Enforcer", new [] {new Vector3(3.2f, 0f, 18.74f), new Vector3(0f, 220f, 0f) } },
-            { "NemesisEnforcer", new [] {new Vector3(3f, 2.28f, 21f), new Vector3(0f, 200f, 0f) } },
+            { "Enforcer", new [] {new Vector3(2f, 0f, 12f), new Vector3(0f, 220f, 0f) } },
+            { "NemesisEnforcer", new [] {new Vector3(4f, 0f, 12f), new Vector3(0f, 170f, 0f) } },
 
-            { "RobHenry", new [] {new Vector3(-4.5f, 1.22f, 8.81f), new Vector3(0f, 128f, 0f) } },
-            { "RobHenry", new [] {new Vector3(-4.5f, 1.22f, 8.81f), new Vector3(0f, 128f, 0f) } },
+            { "RobHenry", new [] {new Vector3(-2f, 0f, 4f), new Vector3(0f, 180f, 0f) } },
+            { "Nemry", new [] {new Vector3(-2f, 0f, 8f), new Vector3(0f, 128f, 0f) } },
 
         };
     }
