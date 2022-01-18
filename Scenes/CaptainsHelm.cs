4c01857d55d6de279447a712464e353e92d28ac6
DestroyedClone <destroyedclone@gmail.com>
2022-01-17 23:54:40 -0800

    Fixed reference

--- Scenes/CaptainsHelm.cs
+++ Scenes/CaptainsHelm.cs
@@ -5,7 +5,7 @@ namespace LobbyAppearanceImprovements.Scenes
     public class CaptainsHelm : LAIScene
     {
         public override string SceneName => "Helm of the Ship/Bridge";
-        public override GameObject BackgroundPrefab => LAIPlugin.CaptainHelmObject;
+        public override GameObject BackgroundPrefab => SceneSetup.CaptainHelmObject;
         public override Vector3 Position => new Vector3(-16.7f, -2f, 8);
         public override Quaternion Rotation => Quaternion.Euler(10f, 179f, 0);
         public override Vector3 Scale => new Vector3(50f, 50f, 50f);
