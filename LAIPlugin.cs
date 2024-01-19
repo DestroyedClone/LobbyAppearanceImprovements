256c85d215ab983a7cb95ef92887fa2c9df6d69c
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-01-19 10:46:39 -0800

    Migrated publicizer to seperate class

--- LAIPlugin.cs
+++ LAIPlugin.cs
@@ -7,8 +7,6 @@ using RoR2;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Security;
-using System.Security.Permissions;
 using UnityEngine;
 using static LobbyAppearanceImprovements.ConfigSetup;
 using static UnityEngine.ColorUtility;
@@ -19,12 +17,6 @@ using TMPro;
 
 
 using RoR2.UI;
-
-[module: UnverifiableCode]
-#pragma warning disable CS0618 // Type or member is obsolete
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
-#pragma warning restore CS0618 // Type or member is obsolete
-//[assembly: HG.Reflection.SearchableAttribute.OptIn]
 
 namespace LobbyAppearanceImprovements
 {
