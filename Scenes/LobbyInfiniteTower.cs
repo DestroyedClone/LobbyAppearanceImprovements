b54117bf1e30ed8214a1226e899abed0c312acb6
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-01-22 11:29:21 -0800

    Added new Scene: LobbyInfiniteTower Added addressable path for lobbybackground for Lobby

--- Scenes/LobbyInfiniteTower.cs
+++ Scenes/LobbyInfiniteTower.cs
@@ -2,11 +2,11 @@
 
 namespace LobbyAppearanceImprovements.Scenes
 {
-    public class Lobby : LAIScene
+    public class LobbyInfiniteTower : LAIScene
     {
         public override string SceneName => "Lobby";
-        public override string SceneNameToken => "LAI_MAP_LOBBY";
-        public override GameObject BackgroundPrefab => LoadAsset("RoR2/Base/Common/DefaultLobbyBackground.prefab");
+        public override string SceneNameToken => "LAI_MAP_LOBBY_INFINITETOWER";
+        public override GameObject BackgroundPrefab => LoadAsset("RoR2/DLC1/Common/LobbyBackgrounds/InfiniteTowerLobbyBackground.prefab");
         public override Vector3 Position => Vector3.zero;
         public override Quaternion Rotation => Quaternion.identity;
         public override Vector3 Scale => Vector3.one;
