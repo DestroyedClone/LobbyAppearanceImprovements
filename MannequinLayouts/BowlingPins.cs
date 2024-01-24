20686185d0b3d56c06bd009a0a39fb9b9e93027e
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-01-24 05:01:08 -0800

    Started preliminary work on overriding survivor mannequins and their positions

---
+++ MannequinLayouts/BowlingPins.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LobbyAppearanceImprovements.MannequinLayouts
+{
+    public class BowlingPins : BaseMannequinLayout
+    {
+    }
+}
