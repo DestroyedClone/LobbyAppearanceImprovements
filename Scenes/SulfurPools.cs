fe497c32d22feb278a5477d96403407eaeafeb1c
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-01-27 07:08:08 -0800

    Updated SulfurPools transforms

--- Scenes/SulfurPools.cs
+++ Scenes/SulfurPools.cs
@@ -8,7 +8,7 @@ namespace LobbyAppearanceImprovements.Scenes
         public override string SceneName => "Hidden Realm: Void Fields";
         public override string SceneNameToken => "MAP_SULFURPOOLS";
         public override GameObject BackgroundPrefab => Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/sulfurpools/SulfurpoolsDioramaDisplay.prefab").WaitForCompletion();
-        public override Vector3 Position => new Vector3(3.5f, -1.5f, 3);
+        public override Vector3 Position => new Vector3(3.5f, -6f, 20f);
         public override Quaternion Rotation => Quaternion.Euler(0f, 0f, 0);
         public override Vector3 Scale => new Vector3(1f, 0.5f, 1f);
     }
