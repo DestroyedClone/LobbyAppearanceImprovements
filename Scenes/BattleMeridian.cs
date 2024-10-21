1effa0e82d2a20f6783927bdbc7b79e7256313b1
DestroyedClone <72328339+DestroyedClone@users.noreply.github.com>
2024-10-21 13:11:09 -0700

    fixed votestart event calling for wrong stage

--- Scenes/BattleMeridian.cs
+++ Scenes/BattleMeridian.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using RoR2;
+using UnityEngine;
 
 namespace LobbyAppearanceImprovements.Scenes
 {
@@ -6,9 +7,9 @@ namespace LobbyAppearanceImprovements.Scenes
     {
         public override string SceneNameToken => "LAI_MAP_BATTLEMERIDIAN";
         public override string SeerToken => "LAI_SEER_BATTLEMERIDIAN";
-        public override GameObject BackgroundPrefab => SceneSetup.CaptainHelmObject;
-        public override Vector3 Position => new Vector3(-16.7f, -3.7f, 8);
-        public override Quaternion Rotation => Quaternion.Euler(0f, 180f, 0);//Quaternion.Euler(10f, 179f, 0);
+        public override GameObject BackgroundPrefab => display;
+        public override Vector3 Position => new(0f, 0f, 0);
+        public override Quaternion Rotation => Quaternion.Euler(0f, 0f, 0);//Quaternion.Euler(10f, 179f, 0);
         public override Vector3 Scale => Vector3.one;
         public override string PreferredLayout => nameof(Layouts.CaptainsHelm_Default);
         public override Material SkyboxOverride => LoadAsset<Material>("RoR2/DLC2/meridian/matSkyboxPMActivate.mat");
@@ -19,23 +20,10 @@ namespace LobbyAppearanceImprovements.Scenes
         public override void Init()
         {
             base.Init();
-            display = PrefabCloneFromAddressable("RoR2/Base/arena/ArenaDioramaDisplay.prefab", "LAI_Scene_Arena");
-            //displayPrefab.transform.position = Position;
-            //displayPrefab.transform.rotation = Rotation;
-            //displayPrefab.transform.localScale = Scale;
-            var gem = CloneFromAddressable("RoR2/Base/arena/Arena_NullifierGemProp.prefab", display.transform);
-            gem.transform.position = new Vector3(3.9f, 0.2f, 8.3f);
-            gem.transform.localScale = Vector3.one * 2;
-            //var portal = CloneFromAddressable("RoR2/Base/PortalArena/PortalArena.prefab", display.transform);
-            //portal.transform.position = new Vector3(9, 0, 28);
-
-            var portal = display.AddComponent<PrefabSpawner>();
-            portal.AssetPath = "RoR2/Base/PortalArena/PortalArena.prefab";
-            portal.localPosition = new Vector3(9, 0, 28);
-            portal.enabled = false;
-
-            var skybox = CloneFromAddressable("RoR2/Base/arena/Arena_Skybox.prefab");
-            skybox.transform.parent = display.transform;
+            display = PrefabCloneFromAddressable("RoR2/DLC2/meridian/Assets/PMCake.prefab", "LAI_Scene_Arena");
+            var gem = CloneFromAddressable("RoR2/DLC2/FalseSon/FalseSonDisplay.prefab", display.transform);
+            gem.transform.position = new Vector3(0f, 0f, 0f);
+            gem.transform.localScale = Vector3.one;
         }
     }
 }
