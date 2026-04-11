0fbf6fa5622d005ad1626fcd26230c094bf67dac
DestroyedClone <DestroyedClone@DESKTOP-7HTR5A9>
2026-04-11 00:13:40 -0700

    Fixed early mod throw by addressing the updated path to text object

--- LAIAssets.cs
+++ LAIAssets.cs
@@ -18,7 +18,7 @@ namespace LobbyAppearanceImprovements
         {
             static async Task Test()
             {
-                var task1 = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/UI/HGCreditNameLabel.prefab");
+                var task1 = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/UI/CreditsPanel/HGCreditNameLabel.prefab");
                 var task2 = Addressables.LoadAssetAsync<TMP_FontAsset>("RoR2/Base/Common/Fonts/Bombardier/tmpBombDropshadow.asset");
                 var task3 = Addressables.LoadAssetAsync<Material>("RoR2/Base/Common/Fonts/Bombardier/tmpBombPlain.mat");
 
